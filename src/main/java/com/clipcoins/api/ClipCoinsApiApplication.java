7e72a2c64743ea3efc2363ec583b7db6300cd24c
vilar <loadslowly@gmail.com>
2024-09-14 18:13:26 +0500

    init: The initial appearance of the ClipCoins API

---
+++ src/main/java/com/clipcoins/api/ClipCoinsApiApplication.java
@@ -0,0 +1,13 @@
+package com.clipcoins.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipCoinsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipCoinsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clipcoins/api/ClipCoinsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clipcoins.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipCoinsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
