2947e7ea9cb7954d6dcef8c4b0f2da2cccc06bcd
vilar <loadslowly@gmail.com>
2024-09-18 17:53:01 +0500

    feat [UserController]: Dependency Injection UserService
    - Completed dependency injection
    - Using the UserService functions inside the UserController
    - A basic ResponseEntity check has been added.

--- src/main/java/com/clipcoins/api/controller/UserController.java
+++ src/main/java/com/clipcoins/api/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.clipcoins.api.controller;
 
 import com.clipcoins.api.model.User;
+import com.clipcoins.api.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,39 +10,86 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private final UserService service;
+
+    public UserController(UserService userService) {
+        this.service = userService;
+    }
 
     @GetMapping("/getAll")
     public ResponseEntity<List<User>> getAllUsers() {
-        return null;
+        List<User> users = service.getAllUsers();
+
+        if (users.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(users);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<User> getUserById(@PathVariable long id) {
-        return null;
+        User user = service.getUserById(id);
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping()
-    public ResponseEntity<User> getUserByTelegramId(@RequestParam String telegramId) {
-        return null;
+    public ResponseEntity<User> getUserByTelegramId(@RequestParam long telegramId) {
+        User user = service.getUserByTelegramId(telegramId);
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping()
     public ResponseEntity<User> getUserByUsername(@RequestParam String username) {
-        return null;
+        User user = service.getUserByUsername(username);
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(user);
     }
 
     @PostMapping()
     public ResponseEntity<User> addUser(@RequestBody User user) {
-        return null;
+        User createdUser = service.addUser(user);
+
+        if (createdUser == null || createdUser.getId() == 0) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(createdUser);
     }
 
     @PutMapping()
     public ResponseEntity<User> updateUser(@RequestBody User user) {
-        return null;
+        User updatedUser = service.updateUser(user);
+
+        if (updatedUser == null || updatedUser.equals(user)) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(updatedUser);
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<User> deleteUser(@PathVariable long id) {
-        return null;
+        User deletedUser = service.deleteUser(id);
+
+        if (deletedUser == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(deletedUser);
     }
 }
