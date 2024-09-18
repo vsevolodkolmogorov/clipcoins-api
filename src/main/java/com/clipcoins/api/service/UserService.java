3c83cfea96cb5d451fee9dd5d4f2d96b6147b528
vilar <loadslowly@gmail.com>
2024-09-18 17:48:00 +0500

    feat [UserService]: Added CRUD implementation for endpoints
    - Added a basic User Service with the implementation of endpoints CRUD

---
+++ src/main/java/com/clipcoins/api/service/UserService.java
@@ -0,0 +1,39 @@
+package com.clipcoins.api.service;
+
+import com.clipcoins.api.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    public User getUserById(long id) {
+        return null;
+    }
+
+    public User getUserByTelegramId(long id) {
+        return null;
+    }
+
+    public User getUserByUsername(String username) {
+        return null;
+    }
+
+    public User addUser(User user) {
+        return null;
+    }
+
+    public User updateUser(User user) {
+        return null;
+    }
+
+    public User deleteUser(Long id) {
+        return null;
+    }
+
+}
