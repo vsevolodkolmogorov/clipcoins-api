2993fdf29c185ce9d48ef2b9de183b73f215510c
vilar <loadslowly@gmail.com>
2024-10-02 13:37:10 +0500

    feat [ValidationUtil]: Added separate Util directory. Added validation logic there

---
+++ src/main/java/com/clipcoins/api/utils/ValidationUtil.java
@@ -0,0 +1,25 @@
+package com.clipcoins.api.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidationUtil {
+
+    public static ResponseEntity<?> checkUser(BindingResult bindingResult) {
+        // Validation of the user by annotation from the model
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errors.put(error.getField(), error.getDefaultMessage());
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        }
+
+        return null;
+    }
+}
